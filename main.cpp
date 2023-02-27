6d9afdd28f63ddaf8657604cbed434c2edda85ad
Yu-Heng Lin <yuhenglin0204@gmail.com>
2023-02-27 02:26:09 -0800

    ok

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int main(int argc, char* argv[]) {
     std::cout << "Number of arguments: " << argc << std::endl;
     std::cout << "Program name: " << argv[0] << std::endl;
-
+    if(argc < 2)
+    {
+        cout << "not enough of argument" << endl;
+        return 0;
+    }
     for (int i = 1; i < argc; i++) {
         std::cout << "Argument " << i << ": " << argv[i] << std::endl;
     }
